d47ffcf9660f4b6403c9a6362ddde596c2253966
Your Name <you@example.com>
2025-04-26 04:10:33 +0300

    a

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ANU.Data;
-
+//yo
 namespace ANU
 {
     public class Startup
